6ae198864e87d31ceb5860e0352e3314cbec0f48
François-Xavier Lemire <francois-xavier.lemire@mail.mcgill.ca>
2018-08-03 10:23:49 -0400

    Host blockrain.js

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use('/', express.static('./tetris'));
 app.use('/assets', express.static('./assets'));
 app.set('env', process.env.NODE_ENV || 'development');
 app.set('port', process.env.PORT || 4000);
